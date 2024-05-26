1f525758543e2c74fbd1c90aaf8812fbc88e96b8
Михаил <madogyyy@gmail.com>
2024-05-26 16:49:56 +0300

    add Grid shared component

---
+++ kidstech/src/shared/ui/Grid/model/types/grid.ts
@@ -0,0 +1,5 @@
+export type GridAutoColumns = 'auto';
+export type GridAutoRows = 'auto';
+export type GridJustify = 'start' | 'end' | 'center' | 'between';
+export type GridAlign = 'start' | 'end' | 'center' | 'stretch';
+export type GridGap = 4 | 8 | 16 | 18 | 24 | 32;
---
+++ kidstech/src/shared/ui/Grid/ui/Grid.tsx
@@ -0,0 +1,73 @@
+import { DetailedHTMLProps, HTMLAttributes, ReactNode } from 'react';
+
+import {
+  GridAlign,
+  GridAutoColumns,
+  GridAutoRows,
+  GridGap,
+  GridJustify
+} from '../model/types/grid';
+import { alignClasses, gapClasses, justifyClasses } from '../model/constants/gridStylesMap';
+
+import cls from './Grid.module.scss';
+import { Mods, classNames } from '@/shared/lib/classNames/classNames';
+
+type DivProps = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>;
+
+export type GridProps = DivProps & {
+  className?: string;
+  children?: ReactNode;
+  gap?: GridGap;
+  templateColumns?: string;
+  templateRows?: string;
+  autoColumns?: GridAutoColumns;
+  autoRows?: GridAutoRows;
+  templateAreas?: string;
+  alignItems?: GridAlign;
+  justifyContent?: GridJustify;
+};
+
+export const Grid = (props: GridProps) => {
+  const {
+    className,
+    children,
+    gap,
+    templateColumns,
+    templateRows,
+    autoColumns,
+    autoRows,
+    templateAreas,
+    alignItems = 'start',
+    justifyContent = 'start',
+    style,
+    ...otherProps
+  } = props;
+
+
+
+  const classes = [
+    className,
+    justifyClasses[justifyContent],
+    alignClasses[alignItems],
+    gap && gapClasses[gap]
+  ];
+
+  const mods: Mods = {};
+
+  return (
+    <div
+      className={classNames(cls.Grid, mods, classes)}
+      style={{
+        ...style,
+        gridTemplateColumns: templateColumns,
+        gridTemplateRows: templateRows,
+        gridAutoColumns: autoColumns,
+        gridAutoRows: autoRows,
+        gridTemplateAreas: templateAreas,
+      }}
+      {...otherProps}
+    >
+      {children}
+    </div>
+  );
+};
